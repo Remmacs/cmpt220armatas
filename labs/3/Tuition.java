8b7f51d9b622de7c110606d27f612bfbb5f80c62
Remmacs <Alex.Armatas1@marist.edu>
2017-02-22 14:23:01 -0500

    Lab 3: Prob: 5.1, 5.7

---
+++ labs/3/Tuition.java
@@ -0,0 +1,16 @@
+
+public class Tuition {
+	public static void main (String args[]) {
+		// tution = tution * 1.05;
+		double tution = 10000;
+		double payment = 0;
+		for(int i = 0; i < 14; i++){
+			tution = tution * 1.05;
+			if(i > 9){
+				payment += tution;
+			}
+		}
+		System.out.println("The total payment will be" + payment);
+	}
+
+}
---
+++ labs/3/countPosNeg.java
@@ -0,0 +1,28 @@
+
+public class countPosNeg {
+	public static void main(String[] args) {
+		int[] intArray = {2,2,2,2};
+		int count = 0;
+		int pos = 0;
+		int neg = 0;
+		int total = 0;
+		
+		do{
+			total += intArray[count];
+			
+			if (intArray[count] >= 0){
+				pos++;
+			}
+			else{
+				neg++;
+			}
+			count++;
+					
+		}while(count < intArray.length);
+		double average = total / count;
+		System.out.println("Positives: " + pos);
+		System.out.println("Negatives: " + neg);
+		System.out.println("Total: " + total);
+		System.out.println("Average: " + average);
+	}
+}
