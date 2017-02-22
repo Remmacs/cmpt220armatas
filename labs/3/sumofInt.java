97b5dee17c01ccb121beec92b3ec3616520aa399
Remmacs <Alex.Armatas1@marist.edu>
2017-02-22 15:35:07 -0500

    6.2

---
+++ labs/3/sumofInt.java
@@ -0,0 +1,27 @@
+
+public class sumofInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// % 10
+		// / 10
+		int theNumber = 456;
+		int count = 0;
+		int total = 0;
+		int intLength = (int) Math.log10(theNumber) + 1;;
+		
+		int[] digitArray = new int[intLength];
+		for(int i = 0; i < intLength; i++){
+			digitArray[count] = theNumber % 10;
+			theNumber = theNumber / 10;
+			count++;
+		}
+		// Now loop through the array and add them together
+		for(int a = 0; a < digitArray.length; a++){
+			total = total + digitArray[a];
+		}
+		System.out.println(total);
+
+	}
+
+}
