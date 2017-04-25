993ca3b4ac25252c7951cd9dc816682011cdbcb4
Remmacs <Alex.Armatas1@marist.edu>
2017-04-25 15:34:21 -0400

    mostly finished problem 9.13 and added title for 9.1

--- labs/6/locationExercise.java
+++ labs/6/locationExercise.java
@@ -12,20 +12,28 @@
 import java.util.Scanner;
 
 public class locationExercise {
+	public static void main(String[] args){
 	//make scanner
 	Scanner input = new Scanner(System.in);
 	//get 2d array from user
-	System.out.println("Give me row and column");
+	System.out.println("Give me row and column in the array");
 	int rows = input.nextInt();
 	int columns = input.nextInt();
 	
 	//making matrix
-	double [][] array = new double[rows][columns];
+	double[][] array = new double[rows][columns];
 	System.out.println("Enter array");
-	for(int i = 0; i<array.length; i++){
-		for(int j=0; j<array[i].length;j++){
+	for(int i = 0; i< array.length; i++){
+		for(int j = 0; j<array[i].length; j++){
 			array[i][j] = input.nextDouble();
 		}
 	}
-
+	Location max = locateLargest(array);
+	//print for user
+	System.out.println("Location of largest element is" + max.maxValue + " at " + max.row + " and " + max.column);
+	
+	}
+	public static Location locateLargest(double[][] x){
+		return new Location(x);
+	}
 }
