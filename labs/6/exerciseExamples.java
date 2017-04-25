2bd87be78b27a0258cb5fbfd1563f0f8aab4695c
Remmacs <Alex.Armatas1@marist.edu>
2017-04-25 15:03:50 -0400

    added the exercise examples 9.1

---
+++ labs/6/exerciseExamples.java
@@ -0,0 +1,21 @@
+
+public class exerciseExamples {
+	public static void main(String[] args){
+		//make the two rectangles
+		Rectangle rectangle1 = new Rectangle(4,40);
+		Rectangle rectangle2 = new Rectangle(3.5,35.9);
+		
+		System.out.println("rectangle 1");
+		System.out.println("Width, Height, Area, Perimeter");
+		System.out.println(rectangle1.width + " " + rectangle1.height + " "
+				+ rectangle1.getArea() + " " + rectangle1.getPerimeter());
+		
+		System.out.println("rectangle 2");
+		System.out.println("Width, Height, Area, Perimeter");
+		System.out.println(rectangle2.width + " " + rectangle2.height + " "
+				+ rectangle2.getArea() + " " + rectangle2.getPerimeter());
+		
+
+	}
+
+}
