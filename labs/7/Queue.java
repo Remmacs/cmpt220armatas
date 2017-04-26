aee2224d16057a5ee67fb598cdd6761b7e557179
Remmacs <Alex.Armatas1@marist.edu>
2017-04-26 14:50:36 -0400

    Started working on skeleton for 10/10

---
+++ labs/7/Queue.java
@@ -0,0 +1,30 @@
+/**
+   * file: Rectangle.java
+   * author: Alexander Armatas
+   * course: CMPT 220
+   * assignment: Lab 7, 10.10
+   * date completed: 4/25/2017
+
+   * 
+   * This file represents the queue
+   *
+   */
+public class Queue {
+	
+	Queue(){
+		
+	}
+	
+	public void enqueue(int v){
+		
+	}
+	
+	public int dequeue(){
+		
+	}
+	
+	public boolean empty(){
+		
+	}
+
+}
