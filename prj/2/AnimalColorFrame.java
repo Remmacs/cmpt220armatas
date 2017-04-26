b2b8bf1560bcc45827ca1bbb8f8b2f9e399924fb
Remmacs <Alex.Armatas1@marist.edu>
2017-04-26 19:48:03 -0400

    Finished first image and changed some functionality

--- prj/2/AnimalColorFrame.java
+++ prj/2/AnimalColorFrame.java
@@ -6,15 +6,22 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+
 import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JSeparator;
 import java.awt.Color;
 import javax.swing.border.LineBorder;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class AnimalColorFrame extends JFrame {
 
 	private JPanel contentPane;
+	public String answer = "";
 
 	/**
 	 * Launch the application.
@@ -42,36 +49,49 @@ public class AnimalColorFrame extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(null);		
+		
+		JLabel imgLabel = new JLabel("");
+		imgLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		imgLabel.setBounds(0, 0, 537, 352);
+		contentPane.add(imgLabel);
 		
 		JComboBox comboBox = new JComboBox();
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent arg0) {
+				if(comboBox.getSelectedItem().toString() == "Cat"){
+					imgLabel.setIcon(new ImageIcon("C:\\Users\\Alex Armatas\\Desktop\\testcat.png"));
+				}
+			}
+		});
+		
 		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 32));
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"", "Dog", "Cat", "Bear"}));
-		comboBox.setBounds(541, 120, 344, 39);
+		comboBox.setBounds(541, 208, 344, 39);
 		contentPane.add(comboBox);
 		
 		JLabel lblChooseAnAnimal = new JLabel("Choose an Animal");
 		lblChooseAnAnimal.setFont(new Font("Tahoma", Font.PLAIN, 29));
-		lblChooseAnAnimal.setBounds(590, 59, 236, 33);
+		lblChooseAnAnimal.setBounds(590, 147, 236, 33);
 		contentPane.add(lblChooseAnAnimal);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBounds(0, 254, 900, 2);
+		separator.setBounds(0, 350, 900, 2);
 		contentPane.add(separator);
 		
 		JLabel lblNewLabel = new JLabel("Animal Colors");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblNewLabel.setBounds(103, 274, 182, 33);
+		lblNewLabel.setBounds(95, 350, 182, 33);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblCorrect = new JLabel("Correct: 0");
 		lblCorrect.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		lblCorrect.setBounds(26, 335, 182, 33);
+		lblCorrect.setBounds(26, 393, 182, 33);
 		contentPane.add(lblCorrect);
 		
 		JLabel lblIncorrect = new JLabel("Incorrect: 0");
 		lblIncorrect.setFont(new Font("Tahoma", Font.PLAIN, 27));
-		lblIncorrect.setBounds(26, 394, 182, 33);
+		lblIncorrect.setBounds(26, 449, 182, 33);
 		contentPane.add(lblIncorrect);
 		
 		JLabel lblWhatColorIs = new JLabel("What Color is the Animal?");
@@ -79,21 +99,15 @@ public class AnimalColorFrame extends JFrame {
 		lblWhatColorIs.setBounds(291, 481, 356, 33);
 		contentPane.add(lblWhatColorIs);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(new LineBorder(new Color(0, 0, 0), 4));
-		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 0, 531, 256);
-		contentPane.add(panel);
-		
 		JLabel lblWhatColorIs_1 = new JLabel("What color is it?");
 		lblWhatColorIs_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblWhatColorIs_1.setBounds(590, 275, 236, 33);
+		lblWhatColorIs_1.setBounds(590, 350, 236, 33);
 		contentPane.add(lblWhatColorIs_1);
 		
 		JComboBox comboBox_1 = new JComboBox();
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"", "Brown", "Black", "Blue", "Red", "Green", "Yellow", "White", "Orange", "Grey", "Pink"}));
 		comboBox_1.setFont(new Font("Tahoma", Font.PLAIN, 32));
-		comboBox_1.setBounds(541, 330, 344, 39);
+		comboBox_1.setBounds(541, 414, 344, 39);
 		contentPane.add(comboBox_1);
 	}
 }
