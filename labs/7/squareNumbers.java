3b40fbb9cf5ec0cef87768f0698b0dc8ee9aa77e
Remmacs <Alex.Armatas1@marist.edu>
2017-04-30 20:54:57 -0400

    Saving progress for lab 10.17

---
+++ labs/7/squareNumbers.java
@@ -0,0 +1,22 @@
+/**
+   * file: Location.java
+   * author: Alexander Armatas
+   * course: CMPT 220
+   * assignment: Lab 7, 10.17
+   * date completed: 4/30/2017
+
+   * 
+   * This file gets the first ten square numbers greater than long.max value.
+   *
+   */
+
+import java.math.BigInteger;
+
+public class squareNumbers {
+	public static void main(String[] args){
+		Double aDouble = Math.sqrt((double)Long.MAX_VALUE);
+		Long firstSquareRoot = aDouble.longValue();
+		BigInteger bigInteger = new BigInteger(firstSquareRoot.toString()).add(BigInteger.ONE);
+	}
+
+}
