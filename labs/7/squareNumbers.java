dad565cf730ce7ecfa2aa1671dbe2bff62549bf5
Remmacs <Alex.Armatas1@marist.edu>
2017-04-30 23:30:53 -0400

    finished lab 7 and prob 10.17

--- labs/7/squareNumbers.java
+++ labs/7/squareNumbers.java
@@ -17,6 +17,11 @@ public class squareNumbers {
 		Double aDouble = Math.sqrt((double)Long.MAX_VALUE);
 		Long firstSquareRoot = aDouble.longValue();
 		BigInteger bigInteger = new BigInteger(firstSquareRoot.toString()).add(BigInteger.ONE);
+		System.out.println(Long.MAX_VALUE);
+		for(int i=0; i<9; i++){
+			System.out.println(bigInteger.multiply(bigInteger).toString());
+			bigInteger = bigInteger.add(BigInteger.ONE);
+		}
 	}
 
 }
